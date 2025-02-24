b72d90298f606d7b7cf53f0db32178151d7d5977
Léo Bernard <leodevian.gh@gmail.com>
2025-02-24 21:49:42 +0100

    feat: add dummy function and dummy test

--- {{ cookiecutter.__project_slug }}/src/{{ cookiecutter.__package_name }}/__init__.py
+++ {{ cookiecutter.__project_slug }}/src/{{ cookiecutter.__package_name }}/__init__.py
@@ -1,5 +1,20 @@
 """{{ cookiecutter.short_description or cookiecutter.project_name }}."""
 
-__all__ = ("__version__",)
+__all__ = (
+    "__version__",
+    "hello",
+)
 
 from {{ cookiecutter.__package_name }}._version import version as __version__
+
+
+def hello(name: str) -> str:
+    """Say hello.
+
+    Args:
+        name: Name.
+
+    Returns:
+        Hello message.
+    """
+    return f"Hello {name}!"
--- {{ cookiecutter.__project_slug }}/tests/test_{{ cookiecutter.__package_name }}.py
+++ {{ cookiecutter.__project_slug }}/tests/test_{{ cookiecutter.__package_name }}.py
@@ -1,1 +1,8 @@
 """Unit tests for `{{ cookiecutter.__package_name }}`."""
+
+from {{ cookiecutter.__package_name }} import hello
+
+
+def test_hello() -> None:
+    """Test the hello message."""
+    assert hello("{{ cookiecutter.author_name }}") == "Hello {{ cookiecutter.author_name }}!"

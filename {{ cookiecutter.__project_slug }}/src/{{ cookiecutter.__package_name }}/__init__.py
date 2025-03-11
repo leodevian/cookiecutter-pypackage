9ccade01a65ce9cfca323b0945f2c175cdc2a8d9
Léo Bernard <leodevian.gh@gmail.com>
2025-03-12 00:30:49 +0100

    feat: add function to get the version from VERSION.txt

--- {{ cookiecutter.__project_slug }}/src/{{ cookiecutter.__package_name }}/__init__.py
+++ {{ cookiecutter.__project_slug }}/src/{{ cookiecutter.__package_name }}/__init__.py
@@ -1,1 +1,20 @@
 """{{ cookiecutter.short_description or cookiecutter.project_name }}."""
+
+from __future__ import annotations
+
+__all__ = ("__version__",)
+
+import importlib.resources
+
+
+def _get_version() -> str:
+    """Read `VERSION.txt` and return its contents.
+
+    Returns:
+        Version string.
+    """
+    files = importlib.resources.files("{{ cookiecutter.__package_name }}")
+    return (files / "VERSION.txt").read_text(encoding="utf-8")
+
+
+__version__ = _get_version()
